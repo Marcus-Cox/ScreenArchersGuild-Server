ed1938fd9065855cac3eb31a18cd8e11ddac6820
Marcus-Cox <marcus.cox92@gmail.com>
2022-12-12 11:02:52 -0600

    added many to many fixtures

--- screenarchersguildapi/models/screenshotcategory.py
+++ screenarchersguildapi/models/screenshotcategory.py
@@ -3,5 +3,5 @@ from .category import Category
 from .screenshot import Screenshot
 
 class ScreenshotCategory(models.Model):
-    category = models.ForeignKey (Category, on_delete=models.CASCADE)
-    screenshot = models.ForeignKey (Screenshot, on_delete=models.CASCADE)
+    category = models.ManyToManyField (Category)
+    screenshot = models.ManyToManyField (Screenshot)
